0010ee78ca03d884f058436ea612036e2b5ec223
rex_lee <lxailcquwer@qq.com>
2018-05-22 15:07:19 +0800

    -

---
+++ src/main/java/com/ppmall/controller/portal/OrderController.java
@@ -0,0 +1,10 @@
+package com.ppmall.controller.portal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+
+}
