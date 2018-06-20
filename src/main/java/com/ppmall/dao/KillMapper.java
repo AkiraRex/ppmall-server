0b8e94512f61ec01c7aa87d31185c855ca792d65
rex_lee <lxailcquwer@qq.com>
2018-06-20 11:31:10 +0800

    kill

--- src/main/java/com/ppmall/dao/KillMapper.java
+++ src/main/java/com/ppmall/dao/KillMapper.java
@@ -6,18 +6,7 @@ import java.util.Map;
 import com.ppmall.pojo.Kill;
 
 public interface KillMapper {
-    int deleteByPrimaryKey(Integer id);
-
-	int insert(Kill record);
-
-	int insertSelective(Kill record);
-
-	Kill selectByPrimaryKey(Integer id);
-
-	int updateByPrimaryKeySelective(Kill record);
-
-	int updateByPrimaryKey(Kill record);
-
+   
 	int deleteByPrimaryKey(Integer id);
 
     int insert(Kill record);
